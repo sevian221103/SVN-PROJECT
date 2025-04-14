d1cff2665a75f7fa84c921a92402d5fce2b9adbd
Sevian <sevianrhyt@gmail.com>
2025-04-14 08:16:08 +0700

    Initial commit

--- start.sh
+++ start.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
+# Jalankan migrasi & cache konfigurasi sebelum serve
 php artisan migrate --force
 php artisan config:cache
 php artisan route:cache
+
+# Jalankan server Laravel
 php artisan serve --host=0.0.0.0 --port=10000
